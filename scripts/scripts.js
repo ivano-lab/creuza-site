31b91d0696cc448f4ae9465b19425481a1a09e5a
Ívano Fontes <ivanofnts@gmail.com>
2025-01-09 18:04:41 -0300

    mobile menu active

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,5 @@ const navMenu = document.getElementById('nav-menu');
 
 mobileMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('active'); 
 });
